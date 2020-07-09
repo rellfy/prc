c249698c04326abc1ec39f1eea5707e5bb96f2cf
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 08:09:24 +1000

    Add controller tests & asmdefs

---
+++ Assets/Tests/Controller.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Unity.Mathematics;
+
+namespace Tests {
+
+    public class Controller {
+
+        [Test]
+        public void ApplyInput_Sprint_True() {
+            InputData input = new InputData() {
+                sprinted = true
+            };
+
+            ControllerInput controller = new ControllerInput();
+
+            ControllerInputProcessor.ApplyInput(
+                ref controller,
+                in input,
+                0
+            );
+
+            Assert.That(controller.sprint == true);
+        }
+
+        [Test]
+        public void ApplyInput_Jump_True() {
+            InputData input = new InputData() {
+                jumped = true
+            };
+
+            ControllerInput controller = new ControllerInput();
+
+            ControllerInputProcessor.ApplyInput(
+                ref controller,
+                in input,
+                0
+            );
+
+            Assert.That(controller.jump == true);
+        }
+
+        [Test]
+        public void ApplyInput_MoveNoSprint_True() {
+            InputData input = new InputData() {
+                cursorMovement = new float2(1f, 1f)
+            };
+
+            const float dt = 1;
+
+            ControllerInput controller = new ControllerInput();
+
+            ControllerInputProcessor.ApplyInput(
+                ref controller,
+                in input,
+                dt
+            );
+
+            Assert.That(
+                controller.cursorPosition.Equals(new float2(2,2))
+            );
+        }
+
+    }
+
+}
