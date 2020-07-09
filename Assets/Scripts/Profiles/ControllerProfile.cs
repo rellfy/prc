160dace5d9bd4f95658eb9145338d6f474fa0901
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-10 04:58:06 +1000

    Add controller profile

---
+++ Assets/Scripts/Profiles/ControllerProfile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Profiles/Controller")]
+public class ControllerProfile : ScriptableObject {
+
+    public float maximumHeight;
+    public float maximumRadius;
+    public float defaultSpeed;
+    public float sprintSpeed;
+
+}
